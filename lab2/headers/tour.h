b4df31a74c0befc97b3394e6c5f54900a6d77306
Alex <n0682736@ntu.ac.uk>
2017-12-03 20:47:16 +0000

    lab2 play

---
+++ lab2/headers/tour.h
@@ -0,0 +1,4 @@
+#ifndef TOUR_H_
+#define TOUR_H_
+
+ENDIF // TOUR_H_
---
+++ lab2/src/tour.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+//#include "tour.h"
+
+using namespace std;
+
+struct timeType
+{
+	int hr;
+	double min;
+	int sec;
+};
+struct tourType
+{
+	string cityName;
+	int distance;
+	timeType travelTime;
+};
+int main()
+{
+	tourType destination;
+	destination.distance = 130;
+	destination.cityName = "Nottingham";
+	destination.travelTime.hr = 3;
+	destination.travelTime.min= 15;
+
+	return 0; 
+}
